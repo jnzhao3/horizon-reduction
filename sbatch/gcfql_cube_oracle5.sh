480316932728cdb915be3fbfdb670dbeb9d62718
jennifer <jennifer-zhao@berkeley.edu>
2025-08-19 15:10:43 -0700

    run commands

--- sbatch/gcfql_cube_oracle5.sh
+++ sbatch/gcfql_cube_oracle5.sh
@@ -3,7 +3,7 @@
 #SBATCH --open-mode=append
 #SBATCH -o /global/scratch/users/jenniferzhao/logs/%A_%a.out
 #SBATCH -e /global/scratch/users/jenniferzhao/logs/%A_%a.err
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mem=64G
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:A5000:1
@@ -20,7 +20,6 @@ JOB_N=1
 COM_ID_S=$((TASK_ID * PARALLEL_N + 1))
 source ~/.bashrc
 micromamba activate aorl
-export PYTHONPATH="../:${PYTHONPATH}"
 
 declare -a commands=(
  [1]='python3 main.py --run_group gcfql_cube_oracle5 --env_name cube-triple-play-oraclerep-v0 --agent ../agents/gcfql.py --dataset_dir ../../scratch/data/cube-triple-play-v0 --train_data_size 100000 --save_dir ../../scratch/gcfql/ --agent.alpha 300 --agent.actor_type best-of-n --json_path ../jsons/data.json'

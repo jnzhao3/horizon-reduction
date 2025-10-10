38b00303ff83883213673324641477a8abc97c14
jennifer <jennifer-zhao@berkeley.edu>
2025-10-10 10:43:25 -0700

    different prior

--- exp.py
+++ exp.py
@@ -2300,7 +2300,7 @@ e2e_maze_7_1 = {
 e2e_maze_7_2 = {
     "script": "e2e.py",
     "priority": "high", # high, normal, low, lowest
-    "time": "24:00:00",
+    "time": "10:00:00",
     "config": {
         "run_group": "e2e_maze_7_2",
         "seed": (0,1,2,3),
